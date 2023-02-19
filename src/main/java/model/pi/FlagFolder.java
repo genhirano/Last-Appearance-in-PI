ee1eac1fa01874777f8767e8e0319a2a56c87b66
genhirano <genhirano@hotmail.com>
2023-02-19 22:53:00 +0900

    発見フラグ アルゴリズム探索 途中、、

---
+++ src/main/java/model/pi/FlagFolder.java
@@ -0,0 +1,21 @@
+package model.pi;
+
+public class FlagFolder {
+
+    private byte[] flagArr = { (byte) 0, (byte) 0};
+
+    private FlagFolder[] childlen;
+
+    private FlagFolder parent;
+
+    public FlagFolder(FlagFolder parent, String data){ //3456
+
+
+        //データが１つだったら末っ子
+
+        //データが2つ以上であれば子あり
+
+
+    }
+
+}
